7fd16c4bbbc467d00f09b2c0508f43385a3e6e07
Jessica Neri <jess.neri94@gmail.com>
2021-01-07 23:09:52 -0300

    Doc: app initialized and api done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    coords: {
+      latitude: 0,
+      longitude: 0,
+    },
+  };
+
+  //will run whenever refresh the browser
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let newCoods = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+        this.setState({ coords: newCoods });
+
+        //api
+        Axios.get(
+          `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=VS6d3fGNOAKH2OeE0Grp2zi5kBTgIymP&q=
+          ${this.state.coords.latitude}%2C${this.state.coords.longitude}`
+        ).then(data => {
+          const resp = data.data.Key
+          console.log(resp)
+          return resp
+        }).then(resp => {
+          Axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${resp}?apikey=VS6d3fGNOAKH2OeE0Grp2zi5kBTgIymP`
+          ).then(res => {
+            console.log(res)
+          })
+        })
+      });
+    }
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
