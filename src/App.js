7a6422808584ad8cff2cc42c4caea7d2ee416a59
Jessica Neri <jess.neri94@gmail.com>
2021-01-10 12:57:30 -0300

    Doc: weather app concluded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Axios from "axios";
 import "./App.css";
+import Weather from "./components/Weather";
 
 class App extends Component {
   state = {
@@ -8,6 +9,8 @@ class App extends Component {
       latitude: 0,
       longitude: 0,
     },
+    data: {},
+    date: {},
   };
 
   //will run whenever refresh the browser
@@ -22,23 +25,41 @@ class App extends Component {
 
         //api
         Axios.get(
-          `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=VS6d3fGNOAKH2OeE0Grp2zi5kBTgIymP&q=
-          ${this.state.coords.latitude}%2C${this.state.coords.longitude}`
-        ).then(data => {
-          const res = data.data.Key
-          return res
-        }).then(res => {
-          Axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${res}?apikey=VS6d3fGNOAKH2OeE0Grp2zi5kBTgIymP`
+          `http://api.weatherapi.com/v1/current.json?key=7cc511b452fa472fa9f190226210801&q=${this.state.coords.latitude},${this.state.coords.longitude}`
           ).then(res => {
+          
+            let weatherData = {
+              name: res.data.location.name,
+              region: res.data.location.region,
+              country: res.data.location.country,
+              description: res.data.current.condition.text,
+              icon: res.data.current.condition.icon,
+              temperature: res.data.current.temp_c,
+              pressure_mb: res.data.current.pressure_mb,
+              uv: res.data.current.uv,
+              humidity: res.data.current.humidity,
+              wind: res.data.current.wind_kph
+
+            }
             console.log(res)
-          })
+          this.setState({data: weatherData})
+
         })
+
+        const today = new Date(),
+        showDate = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+        this.setState({date: showDate})
+
       });
+    }else{
+      console.log("Not Supported")
     }
   }
 
   render() {
-    return <div className="App"></div>;
+    return <div className="App">
+      <Weather weatherData={this.state}/>
+    </div>;
   }
 }
 
---
+++ src/components/Weather.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Weather.css";
+
+
+export default function Weather(props) {
+  const {
+    name,
+    region,
+    country,
+    description,
+    icon,
+    temperature,
+    pressure_mb,
+    uv,
+    humidity,
+    wind,
+  } = props.weatherData.data;
+  const { date } = props.weatherData;
+
+  return (
+    <main>
+      <div className="title">
+        <img src={icon} alt="icon" />
+        <div>
+          <span>
+            {name}, {region}, {country}
+          </span>
+          <h1>{temperature}º C</h1>
+          <h2>{description}</h2>
+        </div>
+      </div>
+      <div className="info">
+          <div className="each-info">
+              <h2>{uv}</h2>
+              <span>UV</span>
+          </div>
+          <div className="each-info">
+              <h2>{pressure_mb} <span>mb</span></h2>
+              <span>Pressure</span>
+          </div>
+          <div className="each-info">
+              <h2>{humidity} <span>%</span></h2>
+              <span>Humidity</span>
+          </div>
+          <div className="each-info">
+              <h2>{wind} <span>km/h</span></h2>
+              <span>Wind</span>
+          </div>
+      </div>
+    </main>
+  );
+}
